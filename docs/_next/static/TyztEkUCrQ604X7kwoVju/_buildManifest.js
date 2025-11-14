f8db75e2a2a7a9677b7230067be74af3926b9d34
Ignacio M. <igmercado46@gmail.com>
2025-11-14 01:50:28 -0300

    maybe

--- docs/_next/static/TyztEkUCrQ604X7kwoVju/_buildManifest.js
+++ docs/_next/static/TyztEkUCrQ604X7kwoVju/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/BeauDev//_next/:path+",
-        "destination": "/BeauDev/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,9 @@
 /** @type {import('next').NextConfig} */
 
-const isProd = process.env.NODE_ENV === "production";
-
 const nextConfig = {
   output: "export",
   distDir: "docs",
-  basePath: isProd ? "/BeauDev" : "",
-  assetPrefix: isProd ? "/BeauDev/" : "",
-  images: {
-    unoptimized: true,
-  },
+  images: { unoptimized: true },
   trailingSlash: true,
 };
 
